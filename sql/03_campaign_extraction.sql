47a5c324c9d169ce647110961488c484fdf7c038
sanjaydp <61502668+sanjaydp@users.noreply.github.com>
2026-05-03 11:11:35 -0400

    Update 03_campaign_extraction.sql

--- sql/03_campaign_extraction.sql
+++ sql/03_campaign_extraction.sql
@@ -6,7 +6,6 @@ WITH scored_population AS (
         cp.first_name,
         cp.last_name,
         cp.state,
-        cp.zip_code,
         ca.credit_score,
         ca.ml_response_score,
         ca.total_debt,
